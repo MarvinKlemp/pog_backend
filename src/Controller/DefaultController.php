b305f437e8c9cb57c489a9e68e733c85e224fa04
MarvinKlemp <mail@marvin-klemp.de>
2016-04-30 11:43:26 +0200

    Json Response

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,18 +4,15 @@ namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="app_init")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        return new JsonResponse([], JsonResponse::HTTP_OK);
     }
 }
